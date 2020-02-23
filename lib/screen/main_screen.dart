b79f69b57f5e0aed246025b07dabdcc17deb9d75
tim118 <tim@vinova.com.sg>
2020-02-23 10:30:27 +0700

    #3 - Refactor UI all app

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hethongchamcong_mobile/screen/checkin/check_in_screen.dart';
+import 'package:hethongchamcong_mobile/screen/more/more_screen.dart';
 
 import 'account/account_screen.dart';
 
@@ -17,11 +18,13 @@ class _MainScreenState extends State<MainScreen> {
   int _selectedPage = 0;
   final PageStorageBucket bucket = PageStorageBucket();
   final _pageOptions = [
-    CheckInLocationPage(  key: PageStorageKey('CheckInScreen'),),
-    Center( child: Text("Leaving screen")),
-    Center( child: Text("Statistic screen")),
-    Center( child: Text("Notification screen")),
-    AccountScreen()
+    CheckInLocationPage(
+      key: PageStorageKey('CheckInScreen'),
+    ),
+    Center(child: Text("Leaving screen")),
+    Center(child: Text("Statistic screen")),
+    Center(child: Text("Notification screen")),
+    MoreScreen()
   ];
 
   @override
@@ -42,38 +45,77 @@ class _MainScreenState extends State<MainScreen> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      body:  IndexedStack(
-        index: _selectedPage,
-        children: _pageOptions,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.red,
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.grey,
-        currentIndex: _selectedPage,
-        onTap: (int index) {
-          setState(() {
-            _selectedPage = index;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-              icon: ImageIcon(AssetImage("./assets/checkin.png")),
-              title: Text('Điểm danh')),
-          BottomNavigationBarItem(
-              icon: ImageIcon(AssetImage("./assets/leaving.png")),
-              title: Text('Xin nghỉ phép')),
-          BottomNavigationBarItem(
-              icon: ImageIcon(AssetImage("./assets/statistics.png")),
-              title: Text('Thống kê')),
-          BottomNavigationBarItem(
-              icon: ImageIcon(AssetImage("./assets/noti.png")),
-              title: Text('Thông báo')),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.more_horiz),
-              title: Text('Thêm')),
+      body: Stack(
+        children: <Widget>[
+          IndexedStack(
+            index: _selectedPage,
+            children: _pageOptions,
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: bottomNavigationBar,
+          ),
         ],
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+
+//      bottomNavigationBar: bottomNavigationBar, // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget get bottomNavigationBar {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xEef7f7f7).withBlue(130),
+        borderRadius: new BorderRadius.only(
+            topLeft: const Radius.circular(20.0),
+            topRight: const Radius.circular(20.0)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey,
+            blurRadius: 20.0, // has the effect of softening the shadow
+            spreadRadius: 5.0, // has the effect of extending the shadow
+            offset: Offset(
+              0.0, // horizontal, move right 10
+              10.0, // vertical, move down 10
+            ),
+          )
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.only(
+            topLeft: const Radius.circular(20.0),
+            topRight: const Radius.circular(20.0)),
+        child: BottomNavigationBar(
+          elevation: 100,
+          type: BottomNavigationBarType.shifting,
+          items: [
+            BottomNavigationBarItem(
+                icon: ImageIcon(AssetImage("./assets/checkin.png")),
+                title: Text('Điểm danh')),
+            BottomNavigationBarItem(
+                icon: ImageIcon(AssetImage("./assets/leaving.png")),
+                title: Text('Xin nghỉ phép')),
+            BottomNavigationBarItem(
+                icon: ImageIcon(AssetImage("./assets/statistics.png")),
+                title: Text('Thống kê')),
+            BottomNavigationBarItem(
+                icon: ImageIcon(AssetImage("./assets/noti.png")),
+                title: Text('Thông báo')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.more_horiz), title: Text('Thêm')),
+          ],
+          selectedItemColor: Colors.lightBlue,
+          unselectedItemColor: Colors.grey,
+          currentIndex: _selectedPage,
+          onTap: (int index) {
+            setState(() {
+              _selectedPage = index;
+            });
+          },
+        ),
+      ),
     );
   }
 }
--- lib/widget/avatar_info_home.dart
+++ lib/widget/avatar_info_home.dart
@@ -11,28 +11,35 @@ class AvatarInfoHome extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             new Container(
-                width: (MediaQuery.of(context).size.width) / 4,
-                height: (MediaQuery.of(context).size.width) / 4,
+                width: (MediaQuery.of(context).size.width) / 4.5,
+                height: (MediaQuery.of(context).size.width) / 4.5,
                 child: ClipRRect(
                     borderRadius: BorderRadius.circular(100.0),
                     child: FadeInImage.assetNetwork(
-                      placeholder: 'assets/login_header.jpg',
-                      image: "https://i.imgur.com/BoN9kdC.png",
+                      placeholder: 'assets/login_header.png',
+                      image: '',
                       fit: BoxFit.cover,
                     )) ,),
             Padding(
               padding: EdgeInsets.only(top: 16),
               child: Text(
                 "Duy Nguyễn",
-                textScaleFactor: 1.5,
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 22,
+                  color: Colors.white
+                ),
               ),
             ),
             Padding(
               padding: EdgeInsets.only(top: 8),
               child: Text(
                 "1612145",
-                textScaleFactor: 1.3,
-                style: TextStyle(color: Colors.black54),
+                style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
+                    color: Colors.white
+                ),
               ),
             )
           ],
